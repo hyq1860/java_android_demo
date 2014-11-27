035faa820df784fcf70fdf27810f59b584c59f22
hyq1860 <pchero@163.com>
2014-11-27 22:51:50 +0800

    伴随内容滚动

--- WeiXinDemo/src/com/applite/weixindemo/MainActivity.java
+++ WeiXinDemo/src/com/applite/weixindemo/MainActivity.java
@@ -14,7 +14,12 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -35,6 +40,13 @@ public class MainActivity extends FragmentActivity {
 	private BadgeView badgeView;
 	private LinearLayout layoutChat;
 	
+	//下划线
+	private ImageView mTabline;
+	
+	//屏幕3分之一变量
+	private int mScreen1_3;
+	//当前页面索引
+	private int mCurrentPageIndex;
 	
 	//Fragment
     @Override
@@ -44,8 +56,26 @@ public class MainActivity extends FragmentActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         
+        //shift+alt+m 可以将一段代码抽取成一个方法
+        initTabline();
+        
         initView();
     }
+
+	private void initTabline() {
+		mTabline = (ImageView)findViewById(R.id.id_iv_tabline);
+        //获取屏幕对象
+        Display display=getWindow().getWindowManager().getDefaultDisplay();
+        //有屏幕的一些信息
+        DisplayMetrics outMetrics=new DisplayMetrics();
+        display.getMetrics(outMetrics);
+        mScreen1_3=outMetrics.widthPixels/3;
+        
+        //注意在那个包里
+        LayoutParams lp= mTabline.getLayoutParams();
+        lp.width=mScreen1_3;
+        mTabline.setLayoutParams(lp);
+	}
 
     private void initView(){
     	
@@ -114,14 +144,39 @@ public class MainActivity extends FragmentActivity {
 					tvContact.setTextColor(Color.GREEN);
 					break;
 				}
+				
+				//设置在那个页面
+				mCurrentPageIndex = position;
 			}
 			
 			
 
 			@Override
-			public void onPageScrolled(int arg0, float arg1, int arg2) {
-				// TODO Auto-generated method stub
-				
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPx) {
+				//position
+				//positionOffset
+				//positionOffsetPx
+				//观察position的值
+				Log.e("Tag", position+" "+positionOffset+" "+positionOffsetPx);
+				//注意强制转换  leftmargin 默认的viewgroup的LayoutParams没有这个属性
+				LinearLayout.LayoutParams  lp= (LinearLayout.LayoutParams)mTabline.getLayoutParams();
+				if(mCurrentPageIndex==0 && position==0)//0=>1
+				{
+					lp.leftMargin=(int)(positionOffset * mScreen1_3 + mCurrentPageIndex*mScreen1_3);
+				}
+				else if(mCurrentPageIndex==1&&position==0)//1=>0
+				{
+					lp.leftMargin=(int)(mCurrentPageIndex * mScreen1_3 + (positionOffset-1)*mScreen1_3);
+				}
+				else if(mCurrentPageIndex==1&&position==1)//1=>2
+				{
+					lp.leftMargin=(int) (mCurrentPageIndex*mScreen1_3+positionOffset*mScreen1_3);
+				}
+				else if(mCurrentPageIndex==2&&position==1)//2=>1
+				{
+					lp.leftMargin=(int)(mCurrentPageIndex*mScreen1_3+(positionOffset-1)*mScreen1_3);
+				}
+				mTabline.setLayoutParams(lp);
 			}
 			
 			@Override
