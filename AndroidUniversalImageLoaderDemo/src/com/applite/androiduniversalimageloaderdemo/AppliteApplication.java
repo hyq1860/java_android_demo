0c9416431f5a5de9e8194f9327912cdf245e5b40
hyq1860 <pchero@163.com>
2014-12-02 18:21:20 +0800

    AndroidUniversalImageLoaderDemo

---
+++ AndroidUniversalImageLoaderDemo/src/com/applite/androiduniversalimageloaderdemo/AppliteApplication.java
@@ -0,0 +1,9 @@
+package com.applite.androiduniversalimageloaderdemo;
+
+
+import android.app.Application;
+import android.content.Context;
+
+public class AppliteApplication extends Application {
+	
+}
