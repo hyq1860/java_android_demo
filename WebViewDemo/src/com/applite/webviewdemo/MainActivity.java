e8881ef181d482b939c08e746f1c020a462495b8
hyq1860 <pchero@163.com>
2014-12-31 22:52:09 +0800

    webviewdemo更新

--- WebViewDemo/src/com/applite/webviewdemo/MainActivity.java
+++ WebViewDemo/src/com/applite/webviewdemo/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -17,6 +20,15 @@ public class MainActivity extends Activity {
 	private String url="http://2014.qq.com";
 	
 	private WebView webView;
+	
+	//刷新按钮
+	private Button btnRefresh;
+	
+	//后退按钮
+	private Button btnBack;
+	
+	//webview的url地址
+	private TextView webUrl;
 	
 	//进度dialog
 	private ProgressDialog progressDialog;
@@ -59,8 +71,30 @@ public class MainActivity extends Activity {
 		
 		return super.onKeyDown(keyCode, event);
 	}
+	
+	class WebViewListener implements View.OnClickListener
+	{
 
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.btn_refresh:
+				webView.reload();
+				break;
+			case R.id.btn_back:
+				webView.goBack();
+				break;
+			default:
+				break;
+			}
+		}
+		
+	}
+	
 	private void init(){
+		btnBack=(Button)findViewById(R.id.btn_back);
+		btnRefresh=(Button)findViewById(R.id.btn_refresh);
+		webUrl=(TextView)findViewById(R.id.tv_webview_url);
 		webView=(WebView)findViewById(R.id.webView);
 		
 		//启用使用javascript
@@ -70,9 +104,19 @@ public class MainActivity extends Activity {
 		//webview缓存的使用（优先使用缓存 默认webview不使用缓存）
 		webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 		
+		btnBack.setOnClickListener(new WebViewListener());
+		btnRefresh.setOnClickListener(new WebViewListener());
 		
 		//页面加载进度
 		webView.setWebChromeClient(new WebChromeClient(){
+			
+			//自定义webview的title
+			@Override
+			public void onReceivedTitle(WebView view, String title) {
+				webUrl.setText(title);
+				super.onReceivedTitle(view, title);
+			}
+			
 			@Override
 			public void onProgressChanged(WebView view, int newProgress) {
 				//newProgress 范围1-100
@@ -106,7 +150,7 @@ public class MainActivity extends Activity {
 
 			private void closeLoadDialog() {
 				// TODO Auto-generated method stub
-				if(progressDialog==null&&progressDialog.isShowing())
+				if(progressDialog!=null && progressDialog.isShowing())
 				{
 					progressDialog.dismiss();
 					progressDialog=null;
